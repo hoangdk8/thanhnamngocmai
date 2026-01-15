f05d2a439fdef4b4293579479521c90013fb83b1
HoangTV <hoang.than@tp.com.vn>
2026-01-15 11:30:56 +0700

    a

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/thanhnamngocmai',
+  assetPrefix: '/thanhnamngocmai/',
+};
+
+module.exports = nextConfig;
